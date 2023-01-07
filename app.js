4077199f427e3ba0e0b872c2f79e970424fe29a1
nikhil.lavan <nikhil.lavan@ladbrokes.com.au>
2023-01-07 19:13:34 +1100

    session 4

--- app.js
+++ app.js
@@ -28,10 +28,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title = (
-  <h1 key="h1" id="title">
-    Namaste React
-  </h1>
+const Title = () => (
+  <div>
+    <h2>Food Villa</h2>
+    <img className="logo" srv="" alt="" ></img>
+  </div>
 )
 
 
@@ -42,16 +43,112 @@ const title = (
 // Functional Component
 // Name starts with capital letter - non mandatory but good practice
 
-const HeaderComponent = () => {
+const Header = () => {
   return (
-    <div>
-      {title      }
-      <h1>Namaste React Functional Component</h1>
-      <h2>This is an H2 tag</h2>
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+
+      </div>
     </div>
   );
 };
 
+const burgerKing = {
+  name : "Burger King",
+  image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+  cusines: ["Burger", "American"],
+  rating: "4.2"
+}
+
+const restroList = [
+  {
+    name : "Burger King",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },
+  {
+    name : "Burger King0",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },  {
+    name : "Burger King1",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },  {
+    name : "Burger King2",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },  {
+    name : "Burger King3",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },  {
+    name : "Burger King4",
+    image: "https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png",
+    cusines: ["Burger", "American"],
+    rating: "4.2"
+  },
+
+]
+
+const RestroCard = ({ name, cusines, rating }) => {
+ // const { name, cusines, rating } = restro
+  return (
+    <div className="card">
+      <img src={burgerKing.image} />
+      <h2>{name}</h2>
+      <h3>{cusines.join(", ")}</h3>
+      <h4>{rating}</h4>
+    </div>
+  )
+
+}
+
+// props - properties
+const Body = () => {
+  return (
+    <div className="restro-list">
+      {
+          restroList.map((restro) => {
+             return  <RestroCard {...restro} />
+          })
+      }
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <h4>Footer</h4>
+  )
+}
+
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
