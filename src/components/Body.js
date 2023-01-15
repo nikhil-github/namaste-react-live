8bb386dd089f2d97ba2bde22ddd0f0d1d7853abb
nikhil.lavan <nikhil.lavan@ladbrokes.com.au>
2023-01-15 15:29:18 +1100

    session 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,39 +1,46 @@
 import { restroList } from "../config";
 import RestroCard from "./Restro";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 // what is state
 // what is a hook - just another function
 // what is usestate
 
-function filterData(searchText, restros) {
-    console.log(restros)
-    return restroList.filter( (restro) => restros.name.includes(searchText));
+function filterData(searchText, allRestros) {
+  console.log(allRestros);
+  return allRestros.filter((restro) => restro.data.name.includes(searchText));
 }
 
-
 const Body = () => {
-  const [ restros, setRestros ] = useState(restroList)
+  const [allRestros, setAllRestros] = useState([]);
+  const [filteredRestros, setFilteredRestros] = useState([]);
   // searchText is a local state variable
   const [searchText, setSearchText] = useState(""); // returns variable name and the set function to update the variable
 
   // called after render with empty array
   // dep arr provided, then call useEffect evert time after render
   useEffect(() => {
-    console.log("call this when dependency is changed")
-    getRestros()
+    console.log("call this when dependency is changed");
+    getRestros();
   }, []);
 
   async function getRestros() {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9678217&lng=80.2185006&page_type=DESKTOP_WEB_LISTING")
-    const json = await data.json()
-    console.log(json)
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9678217&lng=80.2185006&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
     // optional chanining
-    console.log("data from swiggy", json?.data?.cards[2]?.data?.data?.cards)
-    setRestros(json?.data?.cards[2]?.data?.data?.cards)
+    setFilteredRestros(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestros(json?.data?.cards[2]?.data?.data?.cards);
   }
 
-  return (
+  // conditional rendering
+
+  return filteredRestros.length == 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -45,20 +52,21 @@ const Body = () => {
             setSearchText(e.target.value);
           }}
         />
-        <button className="search-btn" onClick={() => {
-               console.log(restros)
-               const data = filterData(searchText, restros);
-               setRestros(data)
-            }
-         }>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, allRestros);
+            setFilteredRestros(data);
+          }}
+        >
           Search
         </button>
       </div>
 
       <div className="restro-list">
-        {restros.map((restro) => {
-        //  console.log("nikhil testing ", restro.data)
-          return <RestroCard {...restro.data} key={restro.data.id}/>;
+        {filteredRestros.map((restro) => {
+          //  console.log("nikhil testing ", restro.data)
+          return <RestroCard {...restro.data} key={restro.data.id} />;
         })}
       </div>
     </>
